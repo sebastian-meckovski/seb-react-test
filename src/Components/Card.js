cb1e8d290585958522a8baeb4f0daa03b2889214
Sebastian Meckovski <Sebastian.Meckovski@streets-heaver.com>
2022-03-01 08:26:55 +0000

    following course

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,17 +2,17 @@ import React from "react";
 import cardPhoto1 from "../images/katie-zaferes.png";
 import star from "../images/star.png"
 
-export default function Card(){
+export default function Card(props){
     return(
         <div className="Card">
             <img src={cardPhoto1} alt="not available" />
             <div>
-                <img src={star} alt="not available" />
-                <span>5.0</span>
-                <span>USA</span>
+                <img src={star} alt="not available" id="cardStar" />
+                <span className="gray">5.0 </span>
+                <span className="gray">usa</span>
             </div>
             <p>Life Lessons with Kaie Zaferes</p>
-            <p> £168 / person</p>
+            <p> <span style={{'fontWeight': 'bold'}} >  £168 </span>/ person</p>
         </div>
     )
 }
